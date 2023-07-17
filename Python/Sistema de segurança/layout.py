f8e9b557181b63a79053e15e3b7736a046d1a087
Josue-Gabriel <103777382+Josue-Gabriel@users.noreply.github.com>
2023-07-17 14:56:29 -0300

    Update layout.py
    Atualização geral do layout, o deixando mais dinâmico e com mais opções. Com a remoção de um layout próprio para a adição de uma nova câmera, para que fique na tela principal o campo para o link de acesso a câmera do celular, junto de uma mensagem indicativa de qual parte do link o usuário deve pegar. Agora podendo ativar e desativar a detecção de movimentos, através de um botão próprio. E por ultimo uma mensagem que avisa quando não é encontrado a câmera que o usuário quer acessar, e aviso quando a nova câmera for adicionada, além claro do preenchimento automático do restante do link de acesso.

--- Python/Sistema de segurança/layout.py
+++ Python/Sistema de segurança/layout.py
@@ -1,37 +1,76 @@
 import PySimpleGUI as sg
+from camera import camera
+from detector_de_movimento import detector_de_movimento
 
-class janela:
-    def layout():
-        url = []
-        ligar = True
-        sg.theme("DarkPurple")
+lista_de_cameras = []
+camera_selecionada = 0
+gravacao = True
+detecção = False
 
-        inicialização = [
-            [sg.Button("Iniciar"), sg.Button("Adicionar cameras")]
-        ]
+sg.theme("DarkPurple")
 
-        adicionar_camera = [
-            [sg.Text("Link da camera")],
-            [sg.Input(key="camera", size=(20, 20))],
-            [sg.Button("Adicionar nova camera")]
-        ]
+layout = [
+    [sg.Text("Sistema de segurança", key="mensagem")],
+    [sg.Input('0.0.0.0:8080', key="cam", size=(55,1))],
+    [sg.Button("Iniciar"), sg.Button("Adicionar câmera"), sg.Button("WebCam", key="webcam"), sg.Button("Detector de movimento", key="detector")],
+    [sg.Text("", key='cameras')]
+]
 
-        layout_one = sg.Window('Inicialização', inicialização)
-        layout_two = sg.Window('Cameras', adicionar_camera)
+janela = sg.Window("", layout)
 
-        while ligar == True:
-            acao, valor = layout_one.read()
-            ligar_adicionar = True
-            if acao == sg.WINDOW_CLOSED:
-                ligar = False
-            if acao == "Iniciar":
-                ligar = False
-            if acao == "Adicionar cameras" and ligar_adicionar == True:
-                    acao, valor = layout_two.read()
-                    if acao == sg.WINDOW_CLOSED:
-                        ligar_adicionar = False
-                    if acao == "Adicionar nova camera":
-                        camera = valor["camera"]
-                        url.append(camera)
+while True:
+    acao, valor = janela.read()
+    if acao == sg.WINDOW_CLOSED:
+        break
+    if acao == "Adicionar câmera":
+        ip_endress = valor['cam']
+        id_camera = f"http://{ip_endress}/video" 
+        lista_de_cameras.append(id_camera)
+        janela['cameras'].update("Câmera adicionada: " + id_camera)
+    if acao == "Iniciar":
+        if detecção == False:
+            try:
+                url = lista_de_cameras[camera_selecionada]
+                quantia_de_cameras = len(lista_de_cameras)
+                while gravacao == True:
+                    gravacao = camera.sistema_seguranca(url)
+                    camera_selecionada += 1
+                    if camera_selecionada == quantia_de_cameras:
+                        camera_selecionada = 0
+                    url = lista_de_cameras[camera_selecionada]
+                gravacao = True
+                janela["cameras"].update("")
+            except:
+                janela['mensagem'].update("Camera não encontrada")
+                lista_de_cameras = []
+                janela["cameras"].update("")
+        else:
+            try:
+                url = lista_de_cameras[camera_selecionada]
+                quantia_de_cameras = len(lista_de_cameras)
+                while gravacao == True:
+                    gravacao = detector_de_movimento.detector(url)
+                    camera_selecionada += 1
+                    if camera_selecionada == quantia_de_cameras:
+                        camera_selecionada = 0
+                    url = lista_de_cameras[camera_selecionada]
+                gravacao = True
+                janela["cameras"].update("")
+            except:
+                janela['mensagem'].update("Camera não encontrada")
+                lista_de_cameras = []
+                janela["cameras"].update("")
+
+    if acao == "webcam":
+        url = 0
+        if detecção == False:
+            camera.sistema_seguranca(url)
+        else: 
+            detector_de_movimento.detector(url)
         
-        return url
+    if acao == "detector":
+        if detecção == False:
+            detecção = True
+        else:
+            detecção = False
+                
