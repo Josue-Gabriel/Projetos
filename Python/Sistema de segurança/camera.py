b82b7a09160158e2f948dde733ac6a641d68bedf
Josue-Gabriel <103777382+Josue-Gabriel@users.noreply.github.com>
2023-07-18 21:06:16 -0300

    Update camera.py
    Adição dos comentários para explicação das partes do código, além da remoção de variáveis avulsas.

--- Python/Sistema de segurança/camera.py
+++ Python/Sistema de segurança/camera.py
@@ -1,31 +1,33 @@
 import cv2
 
 class camera():
-    def sistema_seguranca(url):
+    def sistema_seguranca(url): #Essa função irá receber o link de acesso a câmera do celular
 
-        gravacao = True
-        camera = url
-        captura = cv2.VideoCapture(camera)
+        #Campo para a definição de variáveis
+        gravacao = True #Variável que definira o loop de trocas de câmeras no arquivo layout.py, True indica que o loop continuara, enquanto False indica que a câmera pode ser fechada
+        captura = cv2.VideoCapture(url) #Captura os frames da câmera selecionada
         
         while True:
             
-            ret, quadro = captura.read()
+            ret, quadro = captura.read() #Efetua a leitura dos frames
             
             if not ret:
                 break
             
-            cv2.imshow("Gravações", quadro)
-            
+            cv2.imshow("Gravações", quadro) #Mostra os frames capturados na tela do computador
+
+            #Condição para verificar se a tecla 'q' foi pressionada e fechar a janela e encerra o loop presente no arquivo layout.py
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 gravacao = False
                 break
-            
-            click = cv2.waitKey(5) & 0xFF
-            
-            if click == 0:
+
+            #Condição para verificar se a tecla 'right' (ou no caso a ceta da para a direita do teclado) foi pressionada, e em seguida fechar a janela sem encerrar o loop, para que outra camera seja mostrada
+            if cv2.waitKey(5) & 0xFF == 0::
                 break
-        
+
+        #Libera os dados pegos e fecha as janelas abertas
         captura.release()
         cv2.destroyAllWindows()
 
+        
         return gravacao
